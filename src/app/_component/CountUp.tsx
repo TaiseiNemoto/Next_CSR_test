c7e9aa2a7fad7e788c35cc182cfe19afbf597055
TaiseiNemoto <nemoto@defide-ix.com>
2023-08-17 10:06:39 +0900

    [fix]カウントアップのコンポーネント：型エラーの修正

--- src/app/_component/CountUp.tsx
+++ src/app/_component/CountUp.tsx
@@ -3,19 +3,14 @@
 import { useContext } from "react";
 import { CountContext } from "../countProvider";
 
-type CountType = {
-  count: number,
-  setCount: (value: number) => void
-}
-
 const CountUp = () => {
-  const { count, setCount } = useContext(CountContext)
+  const { count, setCount } = useContext(CountContext);
 
   return (
     <>
       <div>
         <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>カウントアップ</button>
+        <button onClick={() => setCount(num => num + 1)}>カウントアップ</button>
       </div>
     </>
   )
--- src/app/countProvider.tsx
+++ src/app/countProvider.tsx
@@ -4,10 +4,13 @@ import { createContext, useState } from "react"
 
 type CountType = {
   count: number,
-  setCount: (value: number) => void
+  setCount: React.Dispatch<React.SetStateAction<number>>
 }
 
-export const CountContext = createContext({})
+export const CountContext = createContext<CountType>({
+  count: 0,
+  setCount: () => {},
+})
 
 export default function CountProvider({ children }: {
   children: React.ReactNode
